5d94d1c353b65fbbf7a4b2c10f74e13617fd8e81
jordy-sudo <fernando_burbano@hotmail.es>
2020-12-04 12:23:38 -0500

    Segundo Commit 2

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
-//Leer archivo csv
-const csv = require('csv-parser');
-const fs = require('fs');
+/*Comandos
+    --archivo -f: Permite establecer el path del archivo CSV que contiene los datos a analizar
+--pais -c: Permite determinar el país a analizar a través de su código ISO 3166 ALPHA-3. El valor por defecto es “ECU”.
+--anio -y: Permite especificar el año para el cual se requiere las estadísticas. Por defecto, 1960.*/
 
-fs.createReadStream('datos.csv')
-    .pipe(csv())
-    .on('data', (row) => {
-        console.log(row);
-    })
-    .on('end', () => {
-        console.log('Todos los archivos an sido leidos correctamente');
-    });
-    
+const Info = require('./buscador/buscar');
+
+const argv = require('yargs').options({
+    archivo: {
+        alias: 'f',
+        desc: 'Path donde se encuentra el archivo csv',
+        demand: true
+    },
+    pais: {
+        alias: 'c',
+        desc: 'Permite determinar el país a analizar a través de su código ISO 3166 ALPHA-3',
+        demand: true
+    },
+    anio: {
+        alias: 'y',
+        desc: 'Permite especificar el año para el cual se requiere las estadísticas.',
+        demand: true
+    }
+}).argv;
