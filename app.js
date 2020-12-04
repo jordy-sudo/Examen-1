08bb8598a1c3f4eb3dc883ecd351002c84badda2
jordy-sudo <fernando_burbano@hotmail.es>
2020-12-04 13:19:34 -0500

    Tercer commit

--- app.js
+++ app.js
@@ -3,22 +3,36 @@
 --pais -c: Permite determinar el país a analizar a través de su código ISO 3166 ALPHA-3. El valor por defecto es “ECU”.
 --anio -y: Permite especificar el año para el cual se requiere las estadísticas. Por defecto, 1960.*/
 
-const Info = require('./buscador/buscar');
+const Info = require('./buscar');
+
+
 
 const argv = require('yargs').options({
     archivo: {
         alias: 'f',
-        desc: 'Path donde se encuentra el archivo csv',
+        desc: 'Archivo CSV con datos a procesar',
         demand: true
     },
     pais: {
         alias: 'c',
-        desc: 'Permite determinar el país a analizar a través de su código ISO 3166 ALPHA-3',
-        demand: true
+        desc: 'Código del país que se requiere información',
+        demand: true,
+        default: "ECU"
     },
     anio: {
         alias: 'y',
-        desc: 'Permite especificar el año para el cual se requiere las estadísticas.',
-        demand: true
+        desc: 'Año que del que se busca información ',
+        demand: true,
+        default: 1960
     }
 }).argv;
+
+let pais = argv.pais
+let anio = argv.anio
+
+buscar.leerDatos(pais, anio)
+    .then(respuesta => {
+        console.log(`EL pasi es ${pais} y el anio es  ${anio}`);
+    }).catch(err => {
+        console.log(err);
+    });
--- buscador/buscar.js
+++ buscador/buscar.js
@@ -1,5 +1,5 @@
 //Leer archivo csv
-const csv = require('csv-parser');
+/*const csv = require('csv-parser');
 const fs = require('fs');
 
 fs.createReadStream('../datos.csv')
@@ -9,4 +9,46 @@ fs.createReadStream('../datos.csv')
     })
     .on('end', () => {
         console.log('Todos los archivos an sido leidos correctamente');
-    });
+    });*/
+
+////
+
+
+
+
+////
+/*const getInfo = async(pais, anio) => {
+    try {
+        const codigoPais = pais;
+        const respuesta = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${ciudadURI}&appid=${apiKey}`)
+        return respuesta.data.main.temp;
+    } catch (error) {
+        console.log(error.data);
+    }
+
+}*/
+tareasPorHacer = [];
+
+const leerDatos = () => {
+    try {
+        const csv = require('csv-parser');
+        const fs = require('fs');
+
+        fs.createReadStream('../datos.csv')
+            .pipe(csv())
+            .on('data', (row) => {
+                console.log(row);
+                tareasPorHacer = [row]
+            })
+            .on('end', () => {
+                console.log('Todos los archivos an sido leidos correctamente');
+            })
+
+    } catch (error) {
+        tareasPorHacer = [];
+    }
+}
+
+module.exports = {
+    leerDatos
+};
